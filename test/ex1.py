d4a8e092ef2d71c1425232dbeaae54bdcf4188b6
Sai Umakanth <saium@DESKTOP-20BVFJ6>
2019-04-07 18:04:09 +0530

    commit umakanth1

---
+++ test/ex1.py
@@ -0,0 +1,18 @@
+'''
+define
+'''
+
+
+def main():
+    employee = e1\
+    
+    employee( 'name = %s', self.name)
+    employee('mail = %s', self.mail)
+    
+    
+     
+    e1.name = umakanth
+    e1.mail = saiumakanth@gmail.com
+    print(e1.name, e1.salary)
+    
+if __new__='__main__': main()
